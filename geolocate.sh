548d97a341f0bd318ba7df6530c7b67385cf8a64
Franklin Henriquez <franklin.a.henriquez@gmail.com>
2023-10-04 18:29:59 -0400

    refactor: major refactor, starting with parsing args

--- geolocate.sh
+++ geolocate.sh
@@ -3,25 +3,33 @@
 # Name: Franklin Henriquez                                              #
 # Author: Franklin Henriquez (franklin.a.henriquez@gmail.com)           #
 # Creation Date: 04Apr2019                                              #
-# Last Modified: 05Apr2019                                              #
+# Last Modified: 03Oct2023                                              #
 # Description:	Gets location information from mapquest API.			#
 #                                                                       #
 # Version: 1.0.5                                                        #
 #                                                                       #
 #########################################################################
+# Required binaries:
+# - GNU bash 3+
+# - getopt
 
-#set -o errexit
-set -o pipefail
-set -o nounset
-set -o xtrace
+# Notes:
+#
+#
+__version__="0.1.0"
+__author__="Franklin Henriquez"
+__email__="franklin.a.henriquez@gmail.com"
 
 # Set magic variables for current file & dir
 __dir="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 __file="${__dir}/$(basename "${BASH_SOURCE[0]}")"
 __base="$(basename ${__file} .sh)"
-__root="$(cd "$(dirname "${__dir}")" && pwd)" 
+__root="$(cd "$(dirname "${__dir}")" && pwd)"
 
-# DESC: Initialise color variables
+# Script Config Vars
+
+# Color Codes
+# DESC: Initialize color variables
 # ARGS: None
 function echo_color_init(){
 
@@ -60,17 +68,52 @@ function echo_color_init(){
 
 }
 
-# DESC: Generic script initialisation
-# ARGS: None
-function script_init() {
-    # Useful paths
-    readonly orig_cwd="$PWD"
-    readonly script_path="${BASH_SOURCE[0]}"
-    readonly script_dir="$(dirname "$script_path")"
-    readonly script_name="$(basename "$script_path")"
+# Setting up logging
+exec 3>&2 # logging stream (file descriptor 3) defaults to STDERR
+verbosity=3 # default to show warnings
+silent_lvl=0
+crt_lvl=1
+err_lvl=2
+wrn_lvl=3
+inf_lvl=4
+dbg_lvl=5
+bash_dbg_lvl=6
 
-    # Important to always set as we use it in the exit handler
-    readonly ta_none="$(tput sgr0 || true)"
+notify() { log $silent_lvl "${Cyan}NOTE${Color_Off}: $1"; } # Always prints
+critical() { log $crt_lvl "${IRed}CRITICAL:${Color_Off} $1"; }
+error() { log $err_lvl "${Red}ERROR:${Color_Off} $1"; }
+warn() { log $wrn_lvl "${Yellow}WARNING:${Color_Off} $1"; }
+info() { log $inf_lvl "${Blue}INFO:${Color_Off} $1"; } # "info" is already a command
+debug() { log $dbg_lvl "${Purple}DEBUG:${Color_Off} $1"; }
+log() {
+    if [ "${verbosity}" -ge "${1}" ]; then
+        datestring=$(date +'%Y-%m-%d %H:%M:%S')
+        # Expand escaped characters, wrap at 70 chars, indent wrapped lines
+        echo -e "$datestring - __${FUNCNAME[2]}__  - $2" >&3 #| fold -w70 -s | sed '2~1s/^/  /' >&3
+    fi
+}
+
+
+logger() {
+    if [ -n "${LOG_FILE}" ]
+    then
+        echo -e "$1" >> "${log_file}"
+        #echo -e "$1" >> "${LOG_FILE/.log/}"_"$(date +%d%b%Y)".log
+    fi
+}
+
+# DESC: What happens when ctrl-c is pressed
+# ARGS: None
+# Trap ctrl-c and call ctrl_c()
+trap ctrl_c INT
+
+
+function ctrl_c() {
+    info "Trapped CTRL-C signal, terminating script"
+    log "\n================== $(date +'%Y-%m-%d %H:%M:%S'): Run Interrupted  ==================\n"
+    # Any clean up action here
+    # rm -f ${TEMP_FILE}
+    exit 2
 }
 
 # DESC: Usage help
@@ -78,166 +121,165 @@ function script_init() {
 function usage() {
     echo -e "
     \rUsage: ${__base} \"location\" [options]
-    \rDescription:\t Gathers location information given a named location.
+    \rDescription:\t\t\t\t Gathers location information given a named location or zipcode.
 
     \rrequired arguments:
-    \r<location>\tLocation name.
+    \r<location>\t\t\t\t Location name.
 
     \roptional arguments:
-    \r-c|--coordinates\tOnly print coordinates Lat and Lng.
-    \r-h|--help\t\tShow this help message and exit.
-    \r-u|--url\t\tPrint URL for MapQuest. 
+    \r-c|--coordinates\t\t\t Only print coordinates Lat and Lng.
+    \r-h|--help\t\t\t\t Show this help message and exit.
+    \r-l, --log <file>\t\t\t Log file.
+    \r-o|--outfile </path/to/outfile> \t CSV outfile.
+    \r-u|--url\t\t\t\t Print URL for MapQuest.
+    \r-v, --verbose\t\t\t\t Verbosity.
+    \r             \t\t\t\t\t -v info
+    \r             \t\t\t\t\t -vv debug
+    \r             \t\t\t\t\t -vv bash debug
     "
 }
 
-# DESC: Gets if API key is set.
-# ARGS: $@ (required): API key regex varible.
-function check_api_key(){
+# DESC: Parse arguments
+# ARGS: main args
+function parse_args(){
 
-    api_key="${1}"
-    
-    # Validate API key regex.
-    if [[ "$api_key" =~ ^[0-9a-zA-Z]{32}$ ]]
-    then
-        return 0
-    else
-        # Print line number to check variable.
-        variable_line_num=$(grep -n "api_key=" ${__file} | \
-            cut -d ':' -f 1 | head -n 1)
-        echo -e "Please validate ${Red}API Key${Color_Off}: ${api_key}
-                \rReview ${__file} ${IYellow}line number${Color_Off}:" \
-                " ${variable_line_num}"
+    local short_opts='c,h,l:,o:,u,v'
+    local long_opts='coordinates,help,log:,outfile:,url,verbose'
 
-        exit 0
-    fi
-}
-
-# DESC: Print Location info.
-# ARGS: $1 (required): Array of location info as json. 
-#       $2 (optional): Exit code (defaults to 0)
-function print_location_info(){
-
-	info=$1
-
-	# Setting up Location variables
-	CITY=$(echo ${info} | jq -r '.results[0].locations[0] | .adminArea5')
-	STATE=$(echo ${info} | jq -r '.results[0].locations[0] | .adminArea3')
-	COUNTRY=$(echo ${info} | jq -r '.results[0].locations[0] | .adminArea1')
-	#ZIPCODE=${info_arr[3]}
-	LATITUDE=$(echo ${info} | jq -r '.results[0].locations[0] | .latLng.lat')
-	LONGITUDE=$(echo ${info} | jq -r '.results[0].locations[0] | .latLng.lng')
-	MAPURL=$(echo ${info} | jq -r '.results[0].locations[0] | .mapUrl')
-
-	# Print 
-	if [[ ${coordinates} -eq 1 ]]
-	then
-		echo -e "\
-			\rLatitude: \t${LATITUDE}
-			\rLongitude: \t${LONGITUDE}"
-	elif [[ ${mapurl} -eq 1 ]]
-	then
-		echo -e "\
-			\rMapUrl: ${MAPURL}"
-	else
-		echo -e "\
-			\rThe location of ${addr} is:
-			\rCity: \t\t${CITY}
-			\rState: \t\t${STATE}
-			\rCountry: \t${COUNTRY}
-			\rLatitude: \t${LATITUDE}
-			\rLongitude: \t${LONGITUDE}
-			\rMapUrl: \t${MAPURL}"
-	fi
-	return 0
-}
-
-# DESC: Parameter parser
-# ARGS: $@ (optional): Arguments provided to the script
-function parse_params() {
-    local param
-    while [[ $# -gt 0 ]]; do
-        params=$(echo ${1})
-        shift
-        # Iterate through all the parameters.
-        for param in $(echo ${params})
-        do
-            case $param in
-                -c|--coordinates)
-                    coordinates=1
-                    ;;
-               -h|--help)
-                    usage
-                    exit 0
-                    ;;
-               -u|--url)
-                    mapurl=1
-                    ;;
-                -*)
-                    usage
-                    echo -e "${IYellow}Invalid Parameter${Color_Off}:" \
-                    "${IRed}${param}${Color_Off}"
-                    exit 0
-                    ;;
-                *)
-					usage
-                    exit 0
-                    ;;
-                esac
-        done
-    done
-}
-
-# DESC: Main control flow
-# ARGS: $@ (optional): Arguments provided to the script
-function main() {
-    # shellcheck source=source.sh
-    #source "$(dirname "${BASH_SOURCE[0]}")/bash_color_codes"
-
-    #trap "script_trap_err" ERR
-    #trap "script_trap_exit" EXIT
-
-    script_init
-    #colour_init
-    echo_color_init
-
-    # Print usage if no parameters are entered.
-    if [ $# -eq 0 ]
-    then
-        usage
+    #set -x
+    # -use ! and PIPESTATUS to get exit code with errexit set
+    # -temporarily store output to be able to check for errors
+    # -activate quoting/enhanced mode (e.g. by writing out “--options”)
+    # -pass arguments only via   -- "$@"   to separate them correctly
+    ! PARSED=$(getopt --options=${short_opts} --longoptions=${long_opts} --name "$0" -- "$@")
+    if [[ ${PIPESTATUS[0]} -ne 0 ]]; then
+        # e.g. return value is 1
+        #  then getopt has complained about wrong arguments to stdout
+        debug "getopt has complained about wrong arguments"
         exit 2
     fi
-	
-	mapurl=0
-	coordinates=0
 
-	get_params="$@"
-    sorted_params=$( echo ${get_params} | tr ' ' '\n' | grep '-' | sort | tr '\n' ' ' | sed 's/ *$//')
-    parse_params "${sorted_params}"
+    # read getopt’s output this way to handle the quoting right:
+    eval set -- "$PARSED"
 
-    query=$( echo ${get_params} | tr ' ' '\n' | grep -v '-') 
-
-    # Get API key
-    apiKey=""
-
-    # Sourcing the api key to keep it private.
-    if [ -z "${apiKey}" ]
+    if [[ "${PARSED}" == " --" ]]
     then
-        source "${__dir}/mapquest.key"
+        debug "No arguments were passed"
+        usage
+        exit 1
     fi
 
-    # Check API key.
-    check_api_key "${apiKey}"
+    # Getting positional args
+    OLD_IFS=$IFS
+    POSITIONAL_ARGS=${PARSED#*"--"}
+    IFS=' ' read -r -a positional_args <<< "${POSITIONAL_ARGS}"
 
-    api='https://www.mapquestapi.com/geocoding/v1/address?key='
-	args='&location='
-	converter="${api}${apiKey}${args}"
-	addr="$(echo $* | sed 's/ /+/g')" 
+    IFS=$OLD_IFS
 
-	resp=$(curl -s "${converter}${addr}") 
-	
-	print_location_info "${resp}"
-	exit 0
+    # extract options and their arguments into variables.
+    while true ; do
+        case "$1" in
+            -c|--coordinates)
+                 coordinates=1
+                 ;;
+            -h | --help )
+                 # Display usage.
+                 usage
+                 exit 1;
+                 ;;
+            -l | --log)
+                LOG_FILE="$2"
+                log_file="${LOG_FILE/.log/}"_"$(date +%d%b%Y)".log
+                shift 2
+                ;;
+            -o | --outfile)
+                outfile="$2"
+                shift 2
+                ;;
+            -u|--url)
+                 mapurl=1
+                 ;;
+            -v | --verbose)
+               (( verbosity = verbosity + 1 ))
+               if [ $verbosity -eq $bash_dbg_lvl ]
+               then
+                   debug="true"
+               fi
+               shift
+               ;;
+            -- )
+               shift
+               break ;;
+            -*)
+                usage
+                echo -e "${IYellow}Invalid Parameter${Color_Off}:" \
+                "${IRed}${param}${Color_Off}"
+                exit 0
+                ;;
+            * )
+                usage
+                exit 3
+        esac
+    done
+
+    return 0
 }
 
-# Start main function
+# DESC: main
+# ARGS: None
+function main(){
+
+    debug="false"
+    verbose="false"
+
+    echo_color_init
+    parse_args "$@"
+
+    debug "Starting script"
+    debug "
+    out_file:        \t ${outfile}
+    "
+
+    # Getting from parse_args
+    OLD_IFS=$IFS
+    IFS=' ' read -r -a pos_args <<< "${POSITIONAL_ARGS[@]}"
+    IFS=${OLD_IFS}
+
+    pos_arg_count=0
+    len=${#pos_args[@]}
+    if [[ ${len} == 0 ]]
+    then
+        debug "No location was passed."
+        usage
+        exit 1
+    else
+        while [ $pos_arg_count -lt $len ];
+        do
+            echo ${pos_args[$pos_arg_count]}
+            pos_arg_count=$((${pos_arg_count}+1))
+        done
+    fi
+
+    exit 0
+    # Main
+    # Run in debug mode, if set
+    if [ "${debug}" == "true" ]; then
+        set -o noclobber
+        set -o errexit          # Exit on most errors (see the manual)
+        set -o errtrace         # Make sure any error trap is inherited
+        set -o nounset          # Disallow expansion of unset variables
+        set -o pipefail         # Use last non-zero exit code in a pipeline
+        set -o xtrace           # Trace the execution of the script (debug)
+    fi
+
+    if [ ! -z "${outfile:-}" ]; then
+        debug "Testing destination location for write access"
+        touch ${outfile}
+    fi
+
+}
+
+# make it rain
 main "$@"
+debug "Script is complete"
+exit 0
